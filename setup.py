b0d68047ab236b043042f0db2c3b02ea11031fa7
dorosh <dorosshh@gmail.com>
2015-07-22 13:19:23 +0300

    fix

--- setup.py
+++ setup.py
@@ -1,33 +1,21 @@
 import os
 from os.path import join, dirname, split
 #from distutils.core import setup
-from setuptools import setup
-
-
-def path_tokens(path):
-    if not path:
-        return []
-    head, tail = split(path)
-    return path_tokens(head) + [tail]
-
-
-def get_packages():
-    exclude_pacakages = ('__pycache__',)
-    packages = []
-    for path_info in os.walk('src'):
-        tokens = path_tokens(path_info[0])
-        if tokens[-1] not in exclude_pacakages:
-            packages.append('.'.join(tokens))
-    return packages
+from setuptools import setup, find_packages
 
 
 with open('requirements.txt', 'r') as f:
     requirements = f.readlines()
 
 
-setup(name='edx_sso_npoed',
-      version='1.0',
-      install_requires=requirements,
-      py_modules=['pipeline', 'backends'],
-      # packages=get_packages(),
-      )
+setup(
+    name='edx-sso-npoed',
+    version='1.0',
+    description='Client OAuth2 from edX installations',
+    author='edX',
+    url='https://github.com/dorosh/edx-sso-npoed',
+    
+    install_requires=requirements,
+    # py_modules=['pipeline', 'backends'],
+    packages=find_packages(exclude=['tests']),
+)
