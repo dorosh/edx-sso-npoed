f5c97136f23aed0fc141dd82689cfe07704c946d
dorosh <dorosshh@gmail.com>
2015-07-22 12:20:02 +0300

    init

---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+from os.path import join, dirname, split
+# from distutils.core import setup
+from setuptools import setup
+
+
+def path_tokens(path):
+    if not path:
+        return []
+    head, tail = split(path)
+    return path_tokens(head) + [tail]
+
+
+def get_packages():
+    exclude_pacakages = ('__pycache__',)
+    packages = []
+    for path_info in os.walk('src'):
+        tokens = path_tokens(path_info[0])
+        if tokens[-1] not in exclude_pacakages:
+            packages.append('.'.join(tokens))
+    return packages
+
+
+with open('requirements.txt', 'r') as f:
+    requirements = f.readlines()
+
+
+setup(name='edx_sso_npoed',
+      version='1.0',
+      install_requires=requirements,
+      py_modules=['pipeline', 'backends.npoed'],
+      zip_safe=False,
+      # packages=get_packages(),
+      )
